bcf4b855cf7a723a4ef808dd9f953e5cf299c164
David Bleiweiss <dbleiweiss123@gmail.com>
2021-02-27 09:25:10 -0500

    eat a burger app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (name, devoured) VALUES ('Burger 1', true);
+INSERT INTO burgers (name, devoured) VALUES ('Burger 2', true);
+INSERT INTO burgers (name, devoured) VALUES ('Burger 3', true);
